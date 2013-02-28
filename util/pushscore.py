1c73ce4cacba9955518187796d52ccc86c97cdc6
skeezix <skeezix@jellyfish.(none)>
2013-02-28 00:21:23 -0500

    adding util

---
+++ util/pushscore.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import sys
+
+import modulemap
+import singlescore_handler
+
+if len ( sys.argv ) <= 1:
+    print "./foo prid gamename scorenum"
+    print "run from spagserver.py location"
+    sys.exit ( 0 )
+
+req = dict()
+req [ 'prid' ] = sys.argv [ 1 ]
+req [ 'gamename' ] = sys.argv [ 2 ]
+hi = int( sys.argv [ 3 ] )
+
+print "Attempting insert of score %s" % ( hi )
+
+singlescore_handler.update_hi ( req, score_int=hi )
