885659e79112f81b1c791e9369ed5fd29f376236
skeezix <skeezix@flotsam-vm.(none)>
2013-07-13 01:06:22 -0400

    fixing c4a-mgr pnd

--- pnd/make-mgr-pnd.sh
+++ pnd/make-mgr-pnd.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-~/archive/devo/projects/libpnd/testdata/scripts/pnd_make.sh -p compo4all-manager.pnd -d pnd-mgr -x pnd/PXML.xml -i pnd/icon.png
+~/archive/devo/projects/libpnd/testdata/scripts/pnd_make.sh -p compo4all-manager.pnd -d pnd-mgr -x pnd-mgr/PXML.xml -i pnd/icon.png
